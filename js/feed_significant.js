dd08e224b380d5915a1235d2ea909d335c8a8ece
developfor <will@developfor.com>
2014-04-09 13:19:22 -0400

    feed update

--- js/feed_significant.js
+++ js/feed_significant.js
@@ -5,26 +5,27 @@ $.ajax({
 				dataType:'xml',
 				type:'GET',
 				success:function(xml) {
-					var thing = $(xml).find('feed').each(function() {
-						 var title = $(this).find("title").text(); 
-						// var des = $(this).find("description").text();
-						// var link = $(this).find("link").text();
-						// var $des = $('<div class="linkitem"></div>').html(des);
-						// var $link = $('<a></a>').attr('href',link).attr('target','_blank').html(title);
-						// var pubDate = new Date($(this).find("pubDate").text()); 
-						// var day = pubDate.getDate();
-						// var month = pubDate.getMonth() + 1;
-						// var year = pubDate.getFullYear();
-						// var date = day + '/' + month + '/' + year;
-						// var $date = $('<div class="date"></div>').text(date)	
-						// var wrapper = "<li class='single-feed'>";
-						// $(".feed-container").append($(wrapper).append($link,$date,$des));					
-						var hi = "hi"
-						console.log(title);
+
+					var doc = $(xml).find('feed').each(function() {
+						 var title = $(this).find("title"); 
+						// // var des = $(this).find("description").text();
+						// // var link = $(this).find("link").text();
+						// // var $des = $('<div class="linkitem"></div>').html(des);
+						// // var $link = $('<a></a>').attr('href',link).attr('target','_blank').html(title);
+						// // var pubDate = new Date($(this).find("pubDate").text()); 
+						// // var day = pubDate.getDate();
+						// // var month = pubDate.getMonth() + 1;
+						// // var year = pubDate.getFullYear();
+						// // var date = day + '/' + month + '/' + year;
+						// // var $date = $('<div class="date"></div>').text(date)	
+						// // var wrapper = "<li class='single-feed'>";
+						// // $(".feed-container").append($(wrapper).append($link,$date,$des));					
+						// var hi = "hi"
+						 console.log(title[0]);
 					})
 
 					
-					console.log(thing);
+					// console.log(doc[0].childNodes);
 
 				},
 				error:function() {
