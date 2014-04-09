3822fb85fa60db0e7160a2c71c8109449b356802
developfor <will@developfor.com>
2014-04-09 13:09:35 -0400

    feed added xml parser

---
+++ js/feed_significant.js
@@ -0,0 +1,34 @@
+var rssurl = "data/significant_month.atom";
+
+$.ajax({
+				url:rssurl,
+				dataType:'xml',
+				type:'GET',
+				success:function(xml) {
+					var thing = $(xml).find('feed').each(function() {
+						 var title = $(this).find("title").text(); 
+						// var des = $(this).find("description").text();
+						// var link = $(this).find("link").text();
+						// var $des = $('<div class="linkitem"></div>').html(des);
+						// var $link = $('<a></a>').attr('href',link).attr('target','_blank').html(title);
+						// var pubDate = new Date($(this).find("pubDate").text()); 
+						// var day = pubDate.getDate();
+						// var month = pubDate.getMonth() + 1;
+						// var year = pubDate.getFullYear();
+						// var date = day + '/' + month + '/' + year;
+						// var $date = $('<div class="date"></div>').text(date)	
+						// var wrapper = "<li class='single-feed'>";
+						// $(".feed-container").append($(wrapper).append($link,$date,$des));					
+						var hi = "hi"
+						console.log(title);
+					})
+
+					
+					console.log(thing);
+
+				},
+				error:function() {
+					alert("I am sorry, But I can't fetch that feed");
+				}
+			});
+
