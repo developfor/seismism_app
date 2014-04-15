44433f6a93468e271843d6c1e80d05985b591c93
Indie <indira@developfor.com>
2014-04-15 01:00:39 +0100

    buttons added to earthquake map

--- js/map.js
+++ js/map.js
@@ -6,12 +6,12 @@ var mapVisualisation = function(data){
 
 	var margin = {top: 5, left: 5, bottom: 5, right: 5},
 	width = 716 - margin.left - margin.right,
-	height = 335 - margin.top - margin.bottom;
+	height = 300 - margin.top - margin.bottom;
 
 	var projection = d3.geo.mercator()
-		.scale(80)
+		.scale(75)
 		.rotate([210, 0])
-		.translate([width / 2.9, height / 1.8]);
+		.translate([width / 2.9, height / 1.3]);
 
 	var svg = d3.select("#viz-map").append("svg")
 	.attr("width", width)
@@ -38,8 +38,7 @@ var mapVisualisation = function(data){
 				} 
 				else {
 	
-					var mapData = [],
-						tableData = [];
+					var mapData = [];
 
 						data.forEach(function(entry){
 							var mapQuake = {};
@@ -47,16 +46,15 @@ var mapVisualisation = function(data){
 							mapQuake["longitude"] = entry.longitude;
 							mapQuake["mag"] = entry.mag;
 							mapQuake["id"] = entry.id;
+							mapQuake["time"] = entry.time;
+							mapQuake['place'] = entry.place;
 							mapData.push(mapQuake);
 
-							var tableQuake = {};
-							tableQuake["time"] = entry.time;
-							tableQuake["mag"] = entry.mag;
-							tableQuake["id"] = entry.id;
-							tableQuake["place"] = entry.place;
+
 						});
 
 					map(mapData);
+					clicking(mapData);
 
 			    } //END OF DATA ERROR ELSE
 
@@ -72,12 +70,110 @@ var mapVisualisation = function(data){
 		}//END OF MAP ELSE ERROR
 	});  //END OF MAP
 
+
+
+var clicking = function(data_parsed){
+
+		console.log(data_parsed[data.length - 1])
+
+
+
+	     var start = new Date(data_parsed[data.length - 1].time),
+     end = new Date(data_parsed[0].time);
+
+
+
+     var day = 0,
+     magnitude = 1.0,
+     mags = "one";
+
+      $('.button').on('click', function(){
+        var whichId = this.id,
+        data_temp = [];
+        datas = [];
+
+        if (whichId === '0' || whichId === '4' || whichId === '6'){
+
+          day = +whichId;
+          var dateTo = d3.time.day.offset(new Date(start),day);
+
+
+
+
+
+          for(var i = 0; i < data_parsed.length; i++){
+
+            var testDate = new Date(data_parsed[i].time);
+
+
+            if (testDate >= dateTo){
+
+              data_temp.push(data_parsed[i]);
+            }
+          }
+          for(var i = 0; i < data_temp.length; i++){
+
+            
+            if (data_temp[i].mag >= magnitude){
+            
+              datas.push(data_temp[i]);
+            }
+
+          }
+
+          map(datas);
+        }
+
+        if (whichId === 'one'  || whichId === 'four' || whichId === 'five'){
+          mags = whichId;
+          switch(whichId)
+          {
+            case 'one':
+              magnitude = 1.0;
+              break;
+            case 'four':
+              magnitude = 4.5;
+              break;
+            case 'five':
+              magnitude = 5.5;
+              break;
+          }
+
+          var dateTo = d3.time.day.offset(new Date(start),day);
+
+          for(var i = 0; i < data_parsed.length; i++){
+            var testDate = new Date(data_parsed[i].time);
+            if (testDate >= dateTo){
+              data_temp.push(data_parsed[i]);
+            }
+          }
+
+          for(var i = 0; i < data_temp.length; i++){            
+            if (data_temp[i].mag >= magnitude){           
+              datas.push(data_temp[i]);
+            }
+          }
+          sort(datas);
+          map(datas);
+        }
+         $('.button').css("opacity", "0.5");
+
+        $('#'+ day.toString()).css("opacity", '1');
+        $('#' + mags).css('opacity', '1');
+   
+      });
+};
+
 	//START OF MAP FUNCTION
 	function map(data){
 
 		var radius = d3.scale.pow().exponent(expo)
-		.domain([0, d3.max(data, function(d){return d.mag;})])
-		.range([rMin, rMax]);
+			.domain([0, d3.max(data, function(d){ return d.mag; })])
+			.range([rMin, rMax]);
+
+		var color = d3.scale.linear()
+					.domain([d3.min(data, function(d){ return d.mag; }),d3.max(data, function(d){ return d.mag; })])
+					.range(["#FECF03", "#FF4603"]);
   
 
 		var circle = svg.selectAll("circle")   
@@ -93,7 +189,7 @@ var mapVisualisation = function(data){
 			return projection([d.longitude, d.latitude])[1];
 		});
 
-		circle.style("fill", "#0099ba")
+		circle.style("fill", function(d){ return color(d.mag); })
 		.style('opacity', "0.4")
 		.attr("r", function(d){ return radius(d.mag);})
 		.attr("id", function(d){ return d.id });
@@ -101,4 +197,10 @@ var mapVisualisation = function(data){
 
 		circle.exit().remove();
 	}
+
+	function sort(qtable){
+  qtable.sort(function(a, b) {
+        return d3.descending(a.mag, b.mag);
+    });
+}
 };
