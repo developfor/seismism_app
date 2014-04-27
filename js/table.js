d96cf5fa721a8b9f1b2987938bb4db6a1720306a
Indie <indira@developfor.com>
2014-04-27 19:47:08 +0100

    datatable.js implemented scroll pane removed

--- js/table.js
+++ js/table.js
@@ -1,8 +1,5 @@
 var tableVisualisation = function(data){
 
-	var canAnimate = 1;
-	
-
 	var init = function(data){
 		var dataset = [];
 		data.forEach(function(entry){	
@@ -23,97 +20,35 @@ var tableVisualisation = function(data){
 			datum["place"] = thisPlace;
 			dataset.push(datum);
 		});
-		tableSort(dataset);
+
 		makeTable(dataset);
 	};
 
 	var makeTable = function(data){
-
-		console.time("Array initialize");
-		var cTable = $('#table-body');
-
-		$(cTable).empty()
-
-		var table = '<table>'
+		var cBody = $('#table-body-id');
 
 		data.forEach(function(entry){
-			var row = '<tr>' ;
-        
-        // create a new Label Text
-            row += '<td>' + entry.time  + '</td>';
-            row += '<td>' + entry.mag + '</td>';  
-            row += '<td>' + entry.place + '</td>';  
-            row += '<td>' + entry.latitude + ", " + entry.longitude + '</td>';  
-    	row +='</tr>';
-    	table += row;
+			var row = '<tr>';
+			row += '<td>' + entry.time  + '</td>';
+			row += '<td>' + entry.mag + '</td>';  
+			row += '<td>' + entry.place + '</td>';  
+			row += '<td>' + entry.latitude + ", " + entry.longitude + '</td>'; 
 
-    });
-		table += '</table>'
+			row +='</tr>'; 
+			cBody.append(row);
 
-		// data.forEach(function(entry){
-		// 	var row = $('<tr></tr>');
-
-		// 	var col = $('<td></td>').text(entry.time);
-		// 	row.append(col);
-
-		// 	col = $('<td></td>').text(entry.mag);
-		// 	row.append(col);
-
-		// 	col = $('<td></td>').text(entry.place);
-		// 	row.append(col);
-
-		// 	col = $('<td></td>').text(entry.latitude + ", " + entry.longitude);
-		// 	row.append(col);
-
-		// 	table.append(row);
-		// })
-
-		cTable.append(table);
-		canAnimate = 1;
-		console.timeEnd("Array initialize");
-	};
-
-	var sort = function(qtable, sortParameter, direction){
-		qtable.sort(function(a, b) {			
-				return d3[direction](a[sortParameter], b[sortParameter]);
 		});
 
-	
+		$('#table-id').dataTable({
+			"sScrollY": "220px",
+			"sPaginationType": "full_numbers",
+			"iDisplayLength": 100,
+			"bFilter": false,
+			"bLengthChange": false
+		});
+
+
 	};
 
-	var tableSort = function(data){
-		var previous = 'table-time',
-		whichDirection = "descending",
-		directionArray = ["descending", "ascending"],
-		directionSwitch = 0;
-		
-
-
-		$('.table-sort').on('click', function(){
-
-			if (canAnimate === 1){
-				canAnimate = 0;
-				var current = this.id;
-				var sortBy = $(this).attr('id').split('-')[1];
-				if(current != previous){
-					$('.table-sort').css('background-color', '#474747');
-					$('#' + this.id).css('background-color', '#2E5879');
-					sort(data, sortBy, whichDirection);
-					makeTable(data);
-					previous = current;
-					// canAnimate = 1;
-
-				} else{
-					directionSwitch = 1 - directionSwitch;
-					whichDirection = directionArray[directionSwitch];
-					sort(data, sortBy, whichDirection);
-					makeTable(data);
-					// canAnimate = 1;
-
-				}
-			}
-			
-		});
-	}
 	init(data);
 };
