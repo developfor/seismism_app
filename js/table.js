765af64395767b76affd58f9976584a943ccaa5f
Indie <indira@developfor.com>
2014-04-28 01:55:36 +0100

    circle added to mag column in table

--- js/table.js
+++ js/table.js
@@ -25,12 +25,15 @@ var tableVisualisation = function(data){
 	};
 
 	var makeTable = function(data){
+
+
+
 		var cBody = $('#table-body-id');
 
 		data.forEach(function(entry){
 			var row = '<tr>';
 			row += '<td>' + entry.time  + '</td>';
-			row += '<td>' + entry.mag + '</td>';  
+			row += '<td><span class = "table-mag">' + entry.mag + '</span><span class = "circle-table"></span>' + '</td>';  
 			row += '<td>' + entry.place + '</td>';  
 			row += '<td>' + entry.latitude + ", " + entry.longitude + '</td>'; 
 
@@ -38,6 +41,65 @@ var tableVisualisation = function(data){
 			cBody.append(row);
 
 		});
+
+
+		var width = 50,
+		height = 50;
+
+
+		var svg = d3.selectAll(".circle-table")
+      .data(data)
+    .append("svg:svg")
+    .attr('width', width)
+          .attr('height', height);
+
+          svg.append("circle")
+          .attr("r", function(d){return d.mag * 3;})
+          .attr("cx", width/2)
+          .attr("cy", height/2)
+          .attr("fill", "green")
+
+
+
+       // var circile = svg.selectAll("circle")
+       // 		.data
+       //    .enter()
+       //    .append("circle")
+       //    .attr("r", "4")
+          
+
+   //        var circle = svg.selectAll("circle")
+
+
+			// circle.enter()
+			// 	.append("circle");
+			
+
+			// circle.style("fill", "green")
+			// 	.attr("r", "6");
+       
+
+
+
+		// 		var margin = {top:5, right:5, bottom: 5, left:5},
+		// 	width = 200 - margin.right - margin.left,
+		// 	height = 200 - margin.top - margin.bottom;
+
+		// var svg = d3.selectAll(".circle-table").append("svg")
+  //           .attr("width", width + margin.left + margin.right)
+  //           .attr("height", height + margin.top + margin.bottom)
+  //           .append("g")
+  //           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  //          var circle = svg.selectAll("circle")
+		// 		.data(data);
+
+		// 	circle.enter()
+		// 		.append("circle");
+			
+
+		// 	circle.style("fill", "green")
+		// 		.attr("r", "6");
 
 		$('#table-id').dataTable({
 			"sScrollY": "220px",
