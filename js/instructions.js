ace7fb29d1a661a2c40fcb58342fd22953680be3
developfor <will@developfor.com>
2014-05-11 16:09:01 -0400

    update instructions

--- js/instructions.js
+++ js/instructions.js
@@ -1,14 +1,16 @@
 var instructions = function(){
 	// alert("instructions");
-	var clicked = 0
+	var clicked = 0;
 	$("#toggle-instructions").on("click", function(){
 		$("#block-instructions-toggle").slideToggle();
 		if(clicked === 0){
-			$("#toggle-instructions").empty().append("show instructions");
-			clicked = 1
+			$("#toggle-instructions").empty().append("show introduction");
+			$(".info-text-instructions").css({"background-color": "#2E5879","color":"white"} );
+			clicked = 1;
 		}else{
-			$("#toggle-instructions").empty().append("hide instructions");
-			clicked = 0
+			$("#toggle-instructions").empty().append("hide introduction");
+			$(".info-text-instructions").css({"background-color": "#F8BD00","color":"black"} );
+			clicked = 0;
 		}
 	});
 }
